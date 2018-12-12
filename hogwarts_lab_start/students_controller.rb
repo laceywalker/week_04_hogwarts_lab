be2e3d85d57ebdd44dc29c7ebeac9d1b6e35e49e
lacey <hiu@example.com>
2018-12-12 16:09:27 +0000

    finish MVP

--- hogwarts_lab_start/students_controller.rb
+++ hogwarts_lab_start/students_controller.rb
@@ -5,15 +5,50 @@ require_relative('models/house')
 also_reload('./models/*')
 
 # index
+get '/students' do
+  @students = Student.all
+  @houses = House.all
+  erb(:index)
+end
+
+#new
+get '/students/new' do
+  erb(:new)
+end
 
 # show
 
-# new
+get '/students/:id' do
+  @student = Student.find(params[:id])
+  erb(:show)
+end
 
 # create
 
+post '/students' do
+  @student = Student.new(params)
+  @student.save
+  erb(:create)
+end
+
+
 # edit
+get '/students/:id/edit' do
+  @student = Student.find(params[:id])
+  erb(:edit)
+end
 
 # update
+post '/students/:id' do
+  Student.new(params).update
+  redirect to '/students'
+end
+
 
 # destroy
+
+post '/students/:id/delete' do
+  student = Student.find(params[:id])
+  student.delete
+  redirect to '/students'
+end
